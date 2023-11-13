29b424f736b02f575c388fc969609f949c9a58ba
james <jimbdooley@gmail.com>
2023-11-12 20:43:28 -0600

    training data collection setup

---
+++ sim_builds/simCartAI/CMakeFiles/cartAI.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cartAI.dir/cartAI.cpp.o"
+  "CMakeFiles/cartAI.dir/cartAI.cpp.o.d"
+  "cartAI"
+  "cartAI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cartAI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ sim_builds/simCartAI/CMakeFiles/cartAI.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cartAI.
+# This may be replaced when dependencies are built.
