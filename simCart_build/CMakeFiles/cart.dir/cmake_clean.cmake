011e4731635eac90bc5b5dc404719eaf475aad9f
james <jimbdooley@gmail.com>
2023-11-12 18:28:04 -0600

    Pxx control implimented

---
+++ simCart_build/CMakeFiles/cart.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cart.dir/cart.cpp.o"
+  "CMakeFiles/cart.dir/cart.cpp.o.d"
+  "cart"
+  "cart.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cart.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
